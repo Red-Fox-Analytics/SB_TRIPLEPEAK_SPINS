9f17a259f39969fef75365a9133fce0130f0a8fb
Malik Redfox <malik@redfox.io>
2022-05-03 18:48:17 +0000

    backup

--- models/testColumns.sql
+++ models/testColumns.sql
@@ -1,14 +1,18 @@
- {%- set my_list = adapter.get_columns_in_relation(ref('columns')) -%}
-
-
-select
-    {% for item in my_list %}
-        {{ item.name }} as {{ item.name | replace('_', '') }}
-
-        {% if not loop.last %}
-            ,
-        {% endif %}
-    {% endfor %}
-
-
-from {{ ref('columns') }}
+ {% set colsQuery %}
+    Select 'column1' as columName
+    union all
+    Select 'column2' as columName
+    union all
+    Select 'column3' as columName
+{% endset %}
+{% set results = run_query(colsQuery) %}
+{% if execute %}
+{% set results_list = results.columns[0].values() %}
+{% else %}
+{% set results_list = [] %}
+{% endif %}
+select 
+{% for item in results_list %}
+    {{item}} as ATTRIBUTE_{{loop.index}}{%if not loop.last%},{% endif %}
+{% endfor %}
+from {{ ref('sb_triplepeak_spins_lp') }} limit 5
