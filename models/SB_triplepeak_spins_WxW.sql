ef151c2b554b6cf3510451c40b1f97acffd7f671
Malik Redfox <malik@redfox.io>
2022-05-05 21:00:19 +0000

    synched.

--- models/SB_triplepeak_spins_WxW.sql
+++ models/SB_triplepeak_spins_WxW.sql
@@ -22,46 +22,46 @@ select   	"Product Level" as PRODUCT_LEVEL,"Category" as CATEGORY,"Subcategory" 
 			{% for item in results_list %}
     			"{{item}}" as ATTRIBUTE{{loop.index}}{%if not loop.last%},{% endif %}
 			{% endfor %}
-			, sum("Units"						) as UNITS	
-			, sum("Units, Yago"					) as UNITS_YAGO
-			, sum(cast(case when "Dollars" is null then 0 else "Dollars" end  as float)) as DOLLARS
-			, sum(cast(case when "Dollars, Yago" is null then 0 else "Dollars, Yago" end as float)) as DOLLARS_YAGO
-			, sum("Dollars, Promo"						) as DOLLARS_PROMO
-			, sum("Dollars, Promo, Yago"					) as DOLLARS_PROMO_YAGO
-			, sum("Units, Promo"							) as UNITS_PROMO
-			, sum("Units, Promo, Yago"					) as UNITS_PROMO_YAGO
-			, sum("Base Dollars"						) as BASE_DOLLARS
-			, sum("Base Dollars, Yago"					) as BASE_DOLLARS_YAGO
-			, sum("Dollars SPM"						) as DOLLARS_SPM
-			, sum("Dollars SPM, Yago"					) as DOLLARS_SPM_YAGO
-			, sum("Units SPM"						) as UNITS_SPM
-			, sum("Units SPM, Yago"					) as UNITS_SPM_YAGO
+			, sum("Units"						) as UNIT_SALES
+			, sum("Units, Yago"					) as UNIT_SALES_YA
+			, sum(cast(case when "Dollars" is null then 0 else "Dollars" end  as float)) as DOLLAR_SALES
+			, sum(cast(case when "Dollars, Yago" is null then 0 else "Dollars, Yago" end as float)) as DOLLAR_SALES_YA
+			, sum("Dollars, Promo"						) as DOLLAR_SALES_PROMO
+			, sum("Dollars, Promo, Yago"					) as DOLLAR_SALES_PROMO_YA
+			, sum("Units, Promo"							) as UNIT_SALES_PROMO
+			, sum("Units, Promo, Yago"					) as UNIT_SALES_PROMO_YA
+			, sum("Base Dollars"						) as BASE_DOLLAR_SALES
+			, sum("Base Dollars, Yago"					) as BASE_DOLLAR_SALES_YA
+			, sum("Dollars SPM"						) as DOLLAR_SALES_SPM
+			, sum("Dollars SPM, Yago"					) as DOLLAR_SALES_SPM_YA
+			, sum("Units SPM"						) as UNIT_SALES_SPM
+			, sum("Units SPM, Yago"					) as UNIT_SALES_SPM_YA
 			, sum(cast("Dollars" 					as float))-		sum(cast("Base Dollars" 			as float)) as INCREMENTAL_SALES
-			, sum(cast("Dollars, Yago" 				 as float))-	sum(cast("Base Dollars, Yago" 	as float)) as INCREMENTAL_SALES_YAGO
-			, sum("Base Units"								) as BASE_UNITS
-			, sum("Base Units, Yago"						) as BASE_UNITS_YAGO
-			, sum("Base Units, Promo"					) as BASE_UNITS_PROMO
-			, sum("Base Units, Promo, Yago"				) as BASE_UNITS_PROMO_YAGO
-			, sum("Base Dollars, Promo"					) as BASE_DOLLARS_PROMO
-			, sum("Base Dollars, Promo, Yago"					) as BASE_DOLLARS_PROMO_YAGO
+			, sum(cast("Dollars, Yago" 				 as float))-	sum(cast("Base Dollars, Yago" 	as float)) as INCREMENTAL_SALES_YA
+			, sum("Base Units"								) as BASE_UNIT_SALES
+			, sum("Base Units, Yago"						) as BASE_UNIT_SALES_YA
+			, sum("Base Units, Promo"					) as BASE_UNIT_SALES_PROMO
+			, sum("Base Units, Promo, Yago"				) as BASE_UNIT_SALES_PROMO_YA
+			, sum("Base Dollars, Promo"					) as BASE_DOLLAR_SALES_PROMO
+			, sum("Base Dollars, Promo, Yago"					) as BASE_DOLLAR_SALES_PROMO_YA
 			, sum("TDP, Any Promo"									) as TDP_ANY_PROMO
-			, sum("TDP, Any Promo, Yago"							) as TDP_ANY_PROMO_YAGO
+			, sum("TDP, Any Promo, Yago"							) as TDP_ANY_PROMO_YA
 			, sum("TDP"									) as TDP
-			, sum("TDP, Yago"							) as TDP_YAGO
+			, sum("TDP, Yago"							) as TDP_YA
 			, max("Max % ACV"						)     as MAX_ACV 
-			, max("Max % ACV, Yago"				) as MAX_ACV_YAGO
+			, max("Max % ACV, Yago"				) as MAX_ACV_YA
 			, max("Avg % ACV"						)     as AVG_ACV 
-			, max("Avg % ACV, Yago"				) as AVG_ACV_YAGO
+			, max("Avg % ACV, Yago"				) as AVG_ACV_YA
 			, max("Max % ACV"	) - max("Max % ACV, Yago"	) as MAX_ACV_PT_CHG
 			, max("Time Period End Date"				) as LAST_UPDATE_DATE
 			, max("# of Stores"				) as NO_OF_STORES_SELLING
-			, max("# of Stores, Yago"			) as NO_OF_STORES_SELLING_YAGO
+			, max("# of Stores, Yago"			) as NO_OF_STORES_SELLING_YA
 			, max("Max % ACV, Any Promo") AS MAX_ACV_ANY_PROMO
-			, max("Max % ACV, Any Promo, Yago") AS MAX_ACV_ANY_PROMO_YAGO
+			, max("Max % ACV, Any Promo, Yago") AS MAX_ACV_ANY_PROMO_YA
 			, max("Weight Weeks, Any Promo") AS WEIGHT_WEEKS_ANY_PROMO
-			, max("Weight Weeks, Any Promo, Yago") AS WEIGHT_WEEKS_ANY_PROMO_YAGO
+			, max("Weight Weeks, Any Promo, Yago") AS WEIGHT_WEEKS_ANY_PROMO_YA
 			, case when max("Number of Weeks Selling") is null then 0 else max("Number of Weeks Selling") end  as NUMBER_OF_WEEKS_SELLING
-			, case when max("Number of Weeks Selling, Yago") is null then 0 else max("Number of Weeks Selling, Yago") end as NUMBER_OF_WEEKS_SELLING_YAGO
+			, case when max("Number of Weeks Selling, Yago") is null then 0 else max("Number of Weeks Selling, Yago") end as NUMBER_OF_WEEKS_SELLING_YA
 			, avg((case when "SIZE"  is null then null else cast("SIZE"  as float) end) ) as _SIZE
 	from {{ source('TRIPLEPEAK_SB', 'TRIPLEPEAK_SPINS_WXW') }} msly--public.miltons_spins_lp_2y msly 
 	group by "Product Level","Category","Subcategory","Channel/Outlet","Retail Account Level","Brand" , "Product Universe" , "UPC" ,"Description"
